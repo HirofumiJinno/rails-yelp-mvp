d764be42f8f70a7c1fcd4ad42942828d90c30fb3
Hirofumi Jinno <hirofumi.jinno@gmail.com>
2021-11-10 17:01:03 +0900

    initial commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 puts 'Cleaning the db...'
 Restaurant.destroy_all
+Review.destroy_all
 
 puts 'Creating 50 restaunrants...'
 50.times do
@@ -14,7 +15,17 @@ puts 'Creating 50 restaunrants...'
     name: "#{Faker::Hipster.word} #{Faker::Food.dish}",
     address: Faker::Games::SuperMario.location,
     phone_number: Faker::PhoneNumber.phone_number,
-    catetory: Restaurant::CATEGORIES.sample
+    category: Restaurant::CATEGORIES.sample
   )
 end
 puts "...created #{Restaurant.count} restaurants"
+
+puts 'Creating 200 reviews...'
+200.times do
+  Review.create!(
+    content: "#{Faker::TvShows::Friends.quote} #{Faker::TvShows::BigBangTheory.quote}",
+    rating: (1..5).to_a.sample,
+    restaurant: Restaurant.all.sample
+  )
+end
+puts "...created #{Review.count} reviews"
